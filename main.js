b6896ceb66ec9a45b44da40a7fcf1a268a45411d
Blu3wolf <blue@blu3wolf.com>
2018-03-23 22:37:58 +0800

    add some neat console entries

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ var utils = require('utils');
 module.exports.loop = function () {
     
     console.log('Hello World!')
+    console.log(utils.checkDronesRequired())
+    console.log('Youve used ' + (Math.round(Game.cpu.getUsed() * 100) / 100) + ' / ' + Game.cpu.limit)
 }
