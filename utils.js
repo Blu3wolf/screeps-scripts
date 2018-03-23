cb16fa700fb113a7e24e7854a5abe00124e9abb2
Blu3wolf <blue@blu3wolf.com>
2018-03-23 22:25:24 +0800

    an array is not a hash table

--- utils.js
+++ utils.js
@@ -35,7 +35,7 @@ module.exports = {
 				}
 				// droneSpaces now equals number of points around source that are not walls or obstacles
 				// next job is to figure out distance from the spawn to the source
-				var sourcepath = curRoom.findPath(Game.spawns[0].pos, source.pos);
+				var sourcepath = curRoom.findPath(Game.spawns['Spawn1'].pos, source.pos);
 				// assuming drones have WORK, CARRY, MOVE, then travel time is 3 times the path length, and it takes 25 ticks to fill a CARRY
 				var droneRatio = sourcepath.length * 2 / 25;
 				dronesRequired += (droneRatio * droneSpaces);
