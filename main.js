4434ffafae37f84aff379e96be7d1485a1e71957
Blu3wolf <blue@blu3wolf.com>
2018-03-24 00:55:21 +0800

    debug

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ module.exports.loop = function () {
 	
 	for(var source in Memory.sources) {
 		console.log(source);
-		console.log(Memory.sources);
+		console.log(Memory.sources[source]);
 		if(source.harvesters.length < source.dronesRequired) {
 			var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {memory: {source: source}});
 			source.harvesters.push(newName);
